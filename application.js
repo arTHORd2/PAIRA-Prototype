11a1c745426139bf36eee4e5ea2f1c5081d8c4d7
Arthur Wong <z5205512@ad.unsw.edu.au>
2020-07-01 21:26:22 +1000

    Add TODOs for new endpoints

--- application.js
+++ application.js
@@ -13,13 +13,25 @@ app.use(
 app.use(bodyParser.json());
 app.engine('html', require('ejs').renderFile);
 
+// Index - Login page
 app.get('/', function(req, res) {
-  return res.render(200, 'index.html');
+  // For now, just redirect to login page.
+  return res.render(200, 'login.html');
+});
+
+// Login - redirect to the dashboard page if successful
+app.post('/login', function (req, res) {
+  // TODO authenticate and redirect if necessary
+});
+
+// Match - return a list of users that could be a good match
+app.get('/match/:id', async function(req, res) {
+  // TODO Search db for matches
 });
 
 var server = app.listen(8000, function () {
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app listening at http://%s:%s", host, port);
 });
